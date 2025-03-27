3aed0677d23c4c478ce992ea2673ed2359e4d9c4
Mugui-r <yago.rabello@hotmail.com>
2025-03-27 17:30:16 -0300

    added instagram post, my third day

---
+++ src/javascript/script.js
@@ -0,0 +1,14 @@
+@media (max-width: 768px) {
+    .apresentacao__box {
+        flex-direction: column;
+    }
+    
+    .apresentacao__descricao,
+    .swiper-container {
+        width: 100%;
+    }
+    
+    .swiper {
+        max-width: 100%;
+    }
+}
